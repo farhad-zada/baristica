e90a1d8094568f4e168e45483a92c83ae1f6612e
nihadaev <nihadaev@mail.ru>
2024-11-06 22:05:51 +0100

    fix

---
+++ src/pages/productsDetail/ProductsDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ProductsDetail() {
+  return (
+    <div className='flex j-center'>
+        <div className="container">
+            
+        </div>
+    </div>
+  )
+}
