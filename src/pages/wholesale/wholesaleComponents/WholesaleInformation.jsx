093930ef0ae257a8ba6108496312aa7a3f267d8d
lutvaliev <lutvaliyev.r@gmail.com>
2024-11-30 08:40:54 -0800

    wholesale responsive

--- src/pages/wholesale/wholesaleComponents/WholesaleInformation.jsx
+++ src/pages/wholesale/wholesaleComponents/WholesaleInformation.jsx
@@ -10,18 +10,18 @@ const WholesaleInformation = () => {
   return (
     <div className={style.info} style={{backgroundImage: `url(${infoBanner})`}}>
       <div className={`${style.info_section}`}>
-        <div className='flex'>
+        <div className={`${style.info_section_text} flex`}>
           <div>
             {wholesale[lang]?.info?.title?.split('\n\n')?.map((paragraph, index) => (<h1 className={`${style.title} white f84`} key={index}>{paragraph}</h1>))}
           </div>
           {/* <h1 className='white f84'>{wholesale[lang]?.info?.title}</h1> */}
-          <div style={{paddingLeft: "30px"}}>
+          <div style={{paddingLeft: "30px"}} className={style.info_section_description}>
             {wholesale[lang]?.info?.description?.split('\n\n')?.map((paragraph, index) => (<p className='white f28' key={index}>{paragraph}</p>))}
           </div>
         </div>
         <div className={`${style.info_grid}`}>
-          {wholesale[lang]?.info?.list?.map((elem) => (
-            <div className={`${style.info_grid_elem} border16`}>
+          {wholesale[lang]?.info?.list?.map((elem, index) => (
+            <div className={`grid-item ${index === 2 ? `grid-item-full ${style.info_grid_elem} border16` :  `${style.info_grid_elem} border16`}`}>
               <h6 className="f48 f700 white">{elem?.title}</h6>
               <p className='white f28'>{elem?.lable}</p>
             </div>
