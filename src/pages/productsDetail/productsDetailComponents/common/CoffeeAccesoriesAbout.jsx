2246769c6f136cf781361dd0205c81cf18fc6c89
Farhad Zada <farhad.szd@gmail.com>
2025-02-07 00:57:16 +0400

    fix about section

--- src/pages/productsDetail/productsDetailComponents/common/CoffeeAccesoriesAbout.jsx
+++ src/pages/productsDetail/productsDetailComponents/common/CoffeeAccesoriesAbout.jsx
@@ -1,29 +1,19 @@
 import React, { useState } from 'react'
 import styles from './CoffeeAccesoriesAbout.module.css'
+import { useSelector } from "react-redux"
 
-export default function CoffeeAccesoriesAbout() {
-    const [about, setAbout] = useState({
-        longDesc: 'BLEND NIGHTHAWK создан для истинных ценителей эспрессо, которые ищут богатство и сложность в каждом глотке. Его уникальная комбинация бразильских и костариканских зерен обеспечивает отличную балансировку вкусов, оставляя длительное и запоминающееся послевкусие.',
-        details: [
-            {
-                heading: 'Тёмный шоколад: насыщенность и глубина',
-                paragraph: 'Тёмные шоколадные ноты придают эспрессо интенсивность и богатый вкус, создавая ощущение тепла и комфорта.'
-            },
-            {
-                heading: 'Тёмный шоколад: насыщенность и глубина',
-                paragraph: 'Тёмные шоколадные ноты придают эспрессо интенсивность и богатый вкус, создавая ощущение тепла и комфорта.'
-            }
-        ]
-    })
+export default function CoffeeAccesoriesAbout({product}) {
+    const { lang } = useSelector(state => state.baristica)
+    const [about, setAbout] = useState(product.about[lang])
     return (
         <div className={styles.detailSection}>
-            <h2 className='f20 fw400 darkGrey_color'>{about.longDesc}</h2>
+            <h2 className='f20 fw400 darkGrey_color'>{about.preface}</h2>
 
             <div className={styles.detail}>
                 {
-                    about.details.length && about.details.map((section) => (
+                    about.body.length && about.body.map((section) => (
                         <div className="section">
-                            <h3 className='f24 fw700 darkGrey_color'>{section.heading}</h3>
+                            <h3 className='f24 fw700 darkGrey_color'>{section.header}</h3>
                             <p className='darkGrey_color f20 fw400'>{section.paragraph}</p>
                         </div>
                     ))
