3d3d4cbb02fd1d7521db8fcee42cb7cf3e29371e
nihadaev <nihadaev@mail.ru>
2025-02-09 03:07:23 +0100

    coffe machines about

--- src/pages/productsDetail/productsDetailComponents/common/CoffeeAccesoriesAbout.jsx
+++ src/pages/productsDetail/productsDetailComponents/common/CoffeeAccesoriesAbout.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './CoffeeAccesoriesAbout.module.css'
 import { useSelector } from "react-redux"
 
 export default function CoffeeAccesoriesAbout({product}) {
     const { lang } = useSelector(state => state.baristica)
-    const [about, setAbout] = useState(product.about[lang])
+    const [about, setAbout] = useState({})
+
+    useEffect(() => {
+        if(JSON.stringify(product) !== '{}' && lang !=='en'){
+            setAbout(product.about[lang])
+        }
+    },[product])
     return (
         <div className={styles.detailSection}>
             <h2 className='f20 fw400 darkGrey_color'>{about.preface}</h2>
 
             <div className={styles.detail}>
                 {
-                    about.body.length && about.body.map((section) => (
+                    about?.body?.length && about?.body?.map((section) => (
                         <div className="section">
-                            <h3 className='f24 fw700 darkGrey_color'>{section.header}</h3>
-                            <p className='darkGrey_color f20 fw400'>{section.paragraph}</p>
+                            <h3 className='f24 fw700 darkGrey_color'>{section?.header}</h3>
+                            <p className='darkGrey_color f20 fw400'>{section?.paragraph}</p>
                         </div>
                     ))
                 }
