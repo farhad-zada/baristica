a4f4bc96a432a66e87860a09b27724140dc5e547
Farhad Zada <farhad.szd@gmail.com>
2025-07-15 12:26:45 +0400

    idk

--- src/pages/productsDetail/productsDetailComponents/machineAbout/MachineAbout.jsx
+++ src/pages/productsDetail/productsDetailComponents/machineAbout/MachineAbout.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import content from '../../../../content/CoffeMachines.json'
+// import content from '../../../../content/CoffeMachines.json'
 import { useSelector } from 'react-redux'
 import { machineDetailsIcons } from '../../../../assets/machineDetailIcons/machineDetailicons'
 import styles from './machineAbout.module.css'
 import MachineAboutExtra from './MachineAboutExtra'
-const { machines } = content
+// const { machines } = content
 
 export default function MachineAbout({ product }) {
     const { lang } = useSelector(state => state.baristica)
@@ -12,17 +12,17 @@ export default function MachineAbout({ product }) {
     useEffect(() => {
         if (JSON.stringify(product) !== '{}') {
             // Очистка строк от всех скрытых символов (например, разрывов строки)
-            const cleanedProductName = product.name[lang].replace(/\s+/g, ' ').trim();
+            // const cleanedProductName = product.name[lang].replace(/\s+/g, ' ').trim();
 
-            const machine = machines.find((machine) => {
-                // Очистка строки из machines перед сравнением
-                const cleanedMachineName = machine.name.replace(/\s+/g, ' ').trim();
-                if(cleanedMachineName === 'SANREMO F64 OD EVO PRO' && cleanedProductName === 'SANREMO F64 OD EVO PRO'){
-                    return 'SANREMO F64 OD EVO PRO'
-                }
-                return cleanedProductName.includes(cleanedMachineName);
-            });
-            setData(machine);
+            // const machine = machines.find((machine) => {
+            //     // Очистка строки из machines перед сравнением
+            //     const cleanedMachineName = machine.name.replace(/\s+/g, ' ').trim();
+            //     if(cleanedMachineName === 'SANREMO F64 OD EVO PRO' && cleanedProductName === 'SANREMO F64 OD EVO PRO'){
+            //         return 'SANREMO F64 OD EVO PRO'
+            //     }
+            //     return cleanedProductName.includes(cleanedMachineName);
+            // });
+            setData(product.about);
         }
     }, [product, lang]);
 
@@ -30,7 +30,7 @@ export default function MachineAbout({ product }) {
         product?.name[lang]?.includes("MAZZER PHILOS") ? <MachineAboutExtra/>
         : <div className={styles.detailSection}>
             {
-                data && data[lang]?.longDescriptions.map((description) => (
+                data && data[lang]?.preface.map((description) => (
                     <h2 className='f20 fw400 darkGrey_color robotoFont'>{description}</h2>
                 ))
             }
@@ -49,7 +49,7 @@ export default function MachineAbout({ product }) {
                                 }
                             </div>
                             <div className="right">
-                                <h3 className='f24 darkGrey_color fw700'>{section.heading}</h3>
+                                <h3 className='f24 darkGrey_color fw700'>{section.header}</h3>
                                 <p className='darkGrey_color f20 fw400'>{section.paragraph}</p>
                             </div>
                         </div>
