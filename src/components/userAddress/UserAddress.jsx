19054c892b946b9f55599097657eaf9f6f5698d5
Farhad Seyfullazada <farhad.szd@gmail.com>
2026-01-22 20:09:01 +0100

    fix addresses in checkout
    - move addressReducer to a file for multiple use
    - re-introduce selected address for OrderLeft a.k.a. for choosing delivery address

--- src/components/userAddress/UserAddress.jsx
+++ src/components/userAddress/UserAddress.jsx
@@ -8,13 +8,14 @@ import Loading from '../loading/Loading'
 import Error from '../error/Error'
 import { handleApiReqRes } from '../../utils/handleApiReqRes.util'
 
-export default function UserAddress({ content, address, index, dispatch }) {
+export default function UserAddress({ content, address, index, dispatch, radio=false }) {
     const { token } = useSelector(state => state.baristica)
 
     const [edit, setEdit] = useState(false)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
     const [message, setMessage] = useState('Something went wrong')
+
 
     const userService = new UserService()
 
@@ -32,18 +33,38 @@ export default function UserAddress({ content, address, index, dispatch }) {
         }
     }
 
+    const setSelectedAddress = (id) => {
+        dispatch({type: 'SET_SELECTED', payload: id});
+    }
+
+    let highlightedAddress = false;
+    if (!radio) {
+        highlightedAddress = address.isPrimary;
+    } else {
+        highlightedAddress = address.selected;
+    }
     return (
         <div>
             <Loading status={loading} />
             <Error status={error} setStatus={setError} message={message} />
 
             <div
-                className={`${styles.address} ${address.isPrimary ? styles.isPrimary : ''} flex a-center j-between`}
+                className={`${styles.address} ${highlightedAddress ? styles.isPrimary : ''} flex a-center j-between`}
             >
                 <span className={`${styles.index} f20 fw400 robotoFont`}>
                     {index}.
                 </span>
-
+                {radio &&
+                    <span className={styles.circle} onClick={() => setSelectedAddress(address._id)}>
+                        {
+                            address.selected === true
+                                ?
+                                <span className={styles.blueCircle}></span>
+                                :
+                                <></>
+                        }
+                    </span>
+                }
                 <div className={`${styles.address_row} flex a-center j-around w-100`}>
                     <span className={`${styles.city} f20 fw400 robotoFont`}>
                         {address.city}
